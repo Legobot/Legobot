000167280ce987f94ad46b358ca4d9e93c80a54d
Bren Briggs <brebrigg@cisco.com>
2014-07-08 22:50:08 -0400

    Updated examples to clarify API usage and change fcns to match Python convention (func)

--- exampleBot.py
+++ exampleBot.py
@@ -1,18 +1,20 @@
 #!/bin/python
 import Legobot
 
-#simple def that simply prints hello world when called
-def fcnsTest(msgClass):
+#All functions need to accept the message object as input
+
+#simple function that simply prints hello world when called
+def example(msgObj):
 	return "hello World", ""
 
 #slightly more complex def that prints back to the user whatever it gets passed
-def fcnsTest2(msgClass):
+def example2(msgObj):
   if msgClass.arg1:
     return "you told me to say: %s" % msgClass.arg1, "kmccabe2"
   else:
     return "you didn't tell me anything to say", "kmccabe2"
 
-def myLogger(msgClass):
+def myLogger(msgObj):
   #print "line from IRC: %s" % msgClass.splitMessage
   pass
 
@@ -21,14 +23,15 @@ if __name__ == '__main__':
   host = "your.irc.server"
   port = 6667
   nick = "mybot"
-  room = ["#room-a","#room-b"]
+  room = ["#social","#test"] #Must be list, even for one item
 
   #create bot object
   myBot = Legobot.legoBot(host,port,nick,room,myLogger)
   
   #add the functions manually to the bot
-  myBot.addFcns("!hello", fcnsTest)
-  myBot.addFcns("!test", fcnsTest2)
+  #first param is the trigger, second is the name of the function to run on match
+  myBot.addFcns("!hello", example)
+  myBot.addFcns("!test", example2)
 
-  #have bot connect to IRC server and log into room specified
+  #have bot connect to IRC server and log into room(s) specified
   myBot.connect()
