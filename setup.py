e04852d2552b6acfe74a3764af3c6819d0f863d0
Bren Briggs <briggs.brenton@gmail.com>
2016-01-16 09:54:54 -0500

    Cleanup and documentation for publishing to PyPi. Closes #9. Closes #10. Closes #11.

--- setup.py
+++ setup.py
@@ -1,6 +1,36 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='Legobot',
-      version='0.1',
-      py_modules=['Legobot'],
-      )
+setup(
+    name='Legobot',
+
+    version='0.1',
+
+    py_modules=['Legobot'],
+
+    description="A flexible platform for easily creating interactive IRC bots",
+
+    author="Kevin McCabe and Bren Briggs",
+
+    url="https://github.com/bbriggs/Legobot",
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    packages=find_packages(exclude=['contrib', 'docs']),
+
+    #install_requires=['socket','select','string','ssl','threading','Queue','datetime','time','random'],
+)
