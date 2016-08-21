3dbccd92503cd0acf929649b04e75cdd636eb865
Drew Bronson <bronsonboy96@gmail.com>
2016-08-21 17:48:32 -0500

    Concurrency issues fixed.

--- Legobot.py
+++ Legobot.py
@@ -2,30 +2,15 @@ from time import sleep
 
 from Lego import Lego
 import WeatherListener
-import CommandLinePrinter
 import IRCConnector
-import Message
 import threading
 
 lock = threading.Lock()
 baseplate = Lego.start(None, lock)
 
 weather_listener = WeatherListener.WeatherListener.start(baseplate, lock)
-# cli_printer = CommandLinePrinter.CommandLinePrinter.start(baseplate)
-irc_connector = IRCConnector.IRCLego.start('#social', 'TheOperative', 'irc.sithmail.com', baseplate, lock, 6697)
-
+irc_connector = IRCConnector.IRCLego.start('#testing', 'TheOperative', 'irc.sithmail.com', baseplate, lock, 6697, True)
 
 baseplate_proxy = baseplate.proxy()
 baseplate_proxy.children.get().append(weather_listener)
-# baseplate_proxy.children.get().append(cli_printer)
 baseplate_proxy.children.get().append(irc_connector)
-
-
-# metadata_1 = {"source": cli_printer}
-# message_1 = {"text": "!weather", "metadata": metadata_1}
-# message_2 = {"text": "90210", "metadata": metadata_1}
-
-
-# baseplate.tell(message_1)
-# sleep(2)
-# baseplate.tell(message_2)
