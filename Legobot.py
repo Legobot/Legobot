831d63bcf2f083df9cdbfb758e6f4d07619a4a6b
Drew Bronson <bronsonboy96@gmail.com>
2016-08-21 21:17:53 -0500

    Adding documentation to Message class.

--- Legobot.py
+++ Legobot.py
@@ -10,9 +10,9 @@ baseplate = Lego.start(None, lock)
 
 weather_listener = WeatherListener.WeatherListener.start(baseplate, lock)
 irc_connector = IRCConnector.IRCLego.start('#testing', 'TheOperative', 'irc.sithmail.com', baseplate, lock, 6697, True)
-img_search = BingImageSearch.BingImageSearch.start(baseplate, lock)
+# img_search = BingImageSearch.BingImageSearch.start(baseplate, lock)
 
 baseplate_proxy = baseplate.proxy()
 baseplate_proxy.children.get().append(weather_listener)
 baseplate_proxy.children.get().append(irc_connector)
-baseplate_proxy.children.get().append(img_search)
+# baseplate_proxy.children.get().append(img_search)
