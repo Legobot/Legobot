2eb4b3205d8c70bbfe490ad0924724e66366089c
Drew Bronson <bronsonboy96@gmail.com>
2016-08-25 18:50:34 -0500

    Refactoring imports for neatness.

--- Legobot.py
+++ Legobot.py
@@ -2,20 +2,19 @@ import threading
 
 import IRCConnector
 from Lego import Lego
-from Legos import WeatherListener
-from Legos import BingImageSearch
-from Legos import Tip
-from Legos import PrintTips
+from Legos.WeatherListener import WeatherListener
+from Legos.BingImageSearch import BingImageSearch
+from Legos.Tip import Tip
+from Legos.PrintTips import PrintTips
 
 lock = threading.Lock()
 baseplate = Lego.start(None, lock)
 
-weather_listener = WeatherListener.WeatherListener.start(baseplate, lock)
+weather_listener = WeatherListener.start(baseplate, lock)
 irc_connector = IRCConnector.IRCLego.start('#testing', 'TheOperative', 'irc.sithmail.com', baseplate, lock, 6697, True)
-tips = Tip.Tip.start(baseplate, lock)
-tip_printer = PrintTips.PrintTips.start(baseplate, lock)
-
-img_search = BingImageSearch.BingImageSearch.start(baseplate, lock)
+tips = Tip.start(baseplate, lock)
+tip_printer = PrintTips.start(baseplate, lock)
+img_search = BingImageSearch.start(baseplate, lock)
 
 baseplate_proxy = baseplate.proxy()
 baseplate_proxy.children.get().append(weather_listener)
