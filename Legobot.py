093b6ba62d22126f52f7f58ae7b994a824d042d7
Drew Bronson <bronsonboy96@gmail.com>
2016-08-25 19:51:55 -0500

    Adding add_child function to streamline addition of legos.

--- Legobot.py
+++ Legobot.py
@@ -1,6 +1,6 @@
 import threading
 
-import IRCConnector
+from IRCConnector import IRCLego
 from Lego import Lego
 from Legos.WeatherListener import WeatherListener
 from Legos.BingImageSearch import BingImageSearch
@@ -9,17 +9,9 @@ from Legos.PrintTips import PrintTips
 
 lock = threading.Lock()
 baseplate = Lego.start(None, lock)
-
-weather_listener = WeatherListener.start(baseplate, lock)
-irc_connector = IRCConnector.IRCLego.start('#testing', 'TheOperative', 'irc.sithmail.com', baseplate, lock, 6697, True)
-tips = Tip.start(baseplate, lock)
-tip_printer = PrintTips.start(baseplate, lock)
-img_search = BingImageSearch.start(baseplate, lock)
-
 baseplate_proxy = baseplate.proxy()
-baseplate_proxy.children.get().append(weather_listener)
-baseplate_proxy.children.get().append(irc_connector)
-baseplate_proxy.children.get().append(tips)
-baseplate_proxy.children.get().append(tip_printer)
-
-baseplate_proxy.children.get().append(img_search)
+baseplate_proxy.add_child(WeatherListener, baseplate, lock)
+baseplate_proxy.add_child(Tip, baseplate, lock)
+baseplate_proxy.add_child(PrintTips, baseplate, lock)
+baseplate_proxy.add_child(BingImageSearch, baseplate, lock)
+baseplate_proxy.add_child(IRCLego, '#testing', 'TheOperative', 'irc.sithmail.com', baseplate, lock, 6697, True)
