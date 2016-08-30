ca31ffafc28f9f9e8f151ed2042ef264b112d4a3
Bren Briggs <briggs.brenton@gmail.com>
2016-08-30 17:02:25 -0400

    Rearrange dirs for packaging; add Vagrantfile for local testing.

--- Legobot.py
+++ Legobot.py
@@ -1,6 +1,5 @@
 from Legobot.Lego import Lego
 
-<<<<<<< HEAD:Source/Legobot.py
 from Legobot.Legos.BingImageSearch import BingImageSearch
 from Legobot.Legos.IRCConnector import IRCConnector
 from Legobot.Legos.PrintTips import PrintTips
@@ -12,21 +11,6 @@ from Legobot.Legos.XKCD import XKCD
 from Legobot.Legos.Help import Help
 from Legobot.Legos.Commendations import Commendations
 from Legobot.Legos.Commendations import PrintCommendations
-=======
-from Source.Lego import Lego
-
-from Legos.BingImageSearch import BingImageSearch
-from Legos.IRCConnector import IRCConnector
-from Legos.PrintTips import PrintTips
-from Legos.Tip import Tip
-from Legos.WeatherListener import WeatherListener
-from Legos.WikipediaTopFinder import WikipediaTopFinder
-from Legos.Roll import Roll
-from Legos.XKCD import XKCD
-from Legos.Help import Help
-from Legos.Commendations import Commendations
-from Legos.Commendations import PrintCommendations
->>>>>>> cc87117... Fix to run from command line:Legobot.py
 
 # Initialize lock and baseplate
 lock = threading.Lock()
--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from setuptools import setup, find_packages
 
 setup(
