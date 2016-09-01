57f53c485b5052474ee3a91574d29a252fbf165e
Drew Bronson <bronsonboy96@gmail.com>
2016-08-31 20:33:24 -0500

    Add TestConnector and tests for it

---
+++ Legobot/Legos/TestingConnector.py
@@ -0,0 +1,14 @@
+from Legobot.Lego import Lego
+
+
+class TestingConnector(Lego):
+    def __init__(self, baseplate, lock, temp_file='testing_file.tmp'):
+        super().__init__(baseplate, lock)
+        self.temp_file = temp_file
+
+    def listening_for(self, message):
+        return True
+
+    def handle(self, message):
+        with open(self.temp_file, mode='w') as f:
+            f.write(str(message))
