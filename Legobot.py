b731ffefdf2675cf935294d49555a8b13b7f2f4c
Bren Briggs <briggs.brenton@gmail.com>
2016-09-27 22:17:03 -0400

    blah

--- Legobot.py
+++ Legobot.py
@@ -2,17 +2,22 @@ import logging
 import threading
 from Legobot.Lego import Lego
 
-from Legobot.Legos.BingImageSearch import BingImageSearch
 from Legobot.Legos.IRCConnector import IRCConnector
-from Legobot.Legos.PrintTips import PrintTips
-from Legobot.Legos.Tip import Tip
-from Legobot.Legos.WeatherListener import WeatherListener
-from Legobot.Legos.WikipediaTopFinder import WikipediaTopFinder
-from Legobot.Legos.Roll import Roll
-from Legobot.Legos.XKCD import XKCD
 from Legobot.Legos.Help import Help
-from Legobot.Legos.Commendations import Commendations
-from Legobot.Legos.Commendations import PrintCommendations
+from Legobot.Legos.Msync import Audit
+
+# Initialize logging
+# create logger with 'spam_application'
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+loghandler = logging.StreamHandler()
+loghandler.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+loghandler.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(loghandler)
 
 # Initialize lock and baseplate
 lock = threading.Lock()
@@ -20,14 +25,5 @@ baseplate = Lego.start(None, lock)
 baseplate_proxy = baseplate.proxy()
 
 # Add children
-baseplate_proxy.add_child(WeatherListener)
-baseplate_proxy.add_child(Tip)
-baseplate_proxy.add_child(PrintTips)
-baseplate_proxy.add_child(BingImageSearch)
-baseplate_proxy.add_child(IRCConnector, channel='#general', nickname='chatbot', server='foo.slack.com', port=6697, use_ssl=True, username='chatbot', password='example.yourIRCgatewaypass')
-baseplate_proxy.add_child(WikipediaTopFinder)
-baseplate_proxy.add_child(Roll)
-baseplate_proxy.add_child(XKCD)
-baseplate_proxy.add_child(Help)
-baseplate_proxy.add_child(Commendations)
-baseplate_proxy.add_child(PrintCommendations)
+baseplate_proxy.add_child(Audit)
+baseplate_proxy.add_child(IRCConnector, channel='#social', nickname='TheVoxFox', server='irc.sithmail.com', port=6697, use_ssl=True, username=None, password=None)
