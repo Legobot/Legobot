82cd5a0d23e3cb7f0016001adb57a45f2e2f47f3
Bren Briggs <briggs.brenton@gmail.com>
2016-10-27 14:46:16 -0400

    Remove non-standard Legos

--- Legobot/Legos/CommandLinePrinter.py
+++
@@ -1,9 +0,0 @@
-from Legobot import Lego
-
-
-class CommandLinePrinter(Lego):
-    def listening_for(self, message):
-        return str(self) != str(message['metadata']['source'])
-
-    def handle(self, message):
-        print(message['text'])
