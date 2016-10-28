7534f188eb036ad439d323630f90ff19e58f2e68
p4rsec <beckmanseth@gmail.com>
2016-10-28 12:07:45 -0400

    Fix #79: Fixed error

---
+++ Legobot.py
@@ -0,0 +1,25 @@
+import logging
+import threading
+from Legobot.Lego import Lego
+
+from Legobot.Connectors.IRC import IRC 
+from Legobot.Legos.Help import Help
+
+# Initialize lock and master lego
+# All others are considered children of the master/base lego
+lock = threading.Lock()
+master = Lego.start(None, lock)
+# This comes from using Pykka for actors
+# It will be simpler soon
+master_proxy = master.proxy()
+
+# Add children
+master_proxy.add_child(IRC,
+                          channel='#freenode',
+                          nickname='legobot',
+                          server='irc.freenode.net',
+                          port=6697,
+                          use_ssl=True,
+                          username=None,
+                          password=None)
+master_proxy.add_child(Help)
