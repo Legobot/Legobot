e00bd805e519e34ad3504b061ffec0df74866475
Bren Briggs <briggs.brenton@gmail.com>
2016-12-08 23:54:23 -0500

    Update example to match latest Legobot usage

--- examples/exmaplebot.py
+++ examples/exmaplebot.py
@@ -5,6 +5,17 @@ from Legobot.Lego import Lego
 from Legobot.Connectors.IRC import IRC 
 from Legobot.Legos.Help import Help
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(ch)
+
 # Initialize lock and master lego
 # All others are considered children of the master/base lego
 lock = threading.Lock()
@@ -15,9 +26,9 @@ master_proxy = master.proxy()
 
 # Add children
 master_proxy.add_child(IRC,
-                          channel='#freenode',
+                          channels=['#social'],
                           nickname='legobot',
-                          server='irc.freenode.net',
+                          server='irc.sithmail.com',
                           port=6697,
                           use_ssl=True,
                           username=None,
