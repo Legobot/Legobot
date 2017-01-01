02e6cb736986122eea219b646bd17d37630a8445
Bren Briggs <briggs.brenton@gmail.com>
2017-01-01 18:22:36 -0500

    Ignore assert() in Bandit for unit tests

--- Test/test_Message.py
+++ Test/test_Message.py
@@ -8,14 +8,14 @@ class TestMetadata(unittest.TestCase):
     def test_default_init_values(self):
         source = Lego(None, threading.Lock())
         metadata = Metadata(source)
-        assert(metadata.dest is None)
+        assert(metadata.dest is None) # nosec
 
     def test_initialization(self):
         source = Lego(None, threading.Lock())
         dest = Lego(None, threading.Lock())
         metadata = Metadata(source, dest)
-        assert(metadata.source == source)
-        assert(metadata.dest == dest)
+        assert(metadata.source == source) # nosec
+        assert(metadata.dest == dest) # nosec
 
 
 class TestMessage(unittest.TestCase):
@@ -23,15 +23,15 @@ class TestMessage(unittest.TestCase):
         source = Lego(None, threading.Lock())
         metadata = Metadata(source)
         message = Message('a message', metadata)
-        assert(message.text == 'a message')
-        assert(message.metadata == metadata)
+        assert(message.text == 'a message') # nosec
+        assert(message.metadata == metadata) # nosec
         message = Message('a message', metadata, True)
-        assert(message.text == 'a message')
-        assert(message.metadata == metadata)
-        assert(message.should_log)
+        assert(message.text == 'a message') # nosec
+        assert(message.metadata == metadata) # nosec
+        assert(message.should_log) # nosec
 
     def test_default_init_values(self):
         source = Lego(None, threading.Lock())
         metadata = Metadata(source)
         message = Message('a message', metadata)
-        assert(not message.should_log)
+        assert(not message.should_log) # nosec
