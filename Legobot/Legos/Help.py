ac25f78cf0b6cf14354498c2e10aa4eac3373b6a
Cody <cody@quantifiedcode.com>
2017-02-15 01:24:39 +0000

    Migrated `%` string formating

--- Legobot/Legos/Help.py
+++ Legobot/Legos/Help.py
@@ -13,7 +13,7 @@ class Help(Lego):
             try:
                 return message['text'].split()[0] == '!help'
             except Exception as e:
-                logger.error('Help lego failed to check message text: %s' % e)
+                logger.error('Help lego failed to check message text: {0!s}'.format(e))
                 return False
 
     def handle(self, message):
