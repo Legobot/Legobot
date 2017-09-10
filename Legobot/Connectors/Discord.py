d30156c1ec699ce2f99687ace85b3a9621b2566d
Bren Briggs <briggs.brenton@gmail.com>
2017-09-10 14:40:43 -0400

    Ignore messages from ourself in Discord

--- Legobot/Connectors/Discord.py
+++ Legobot/Connectors/Discord.py
@@ -1,5 +1,4 @@
 from websocket import create_connection
-import datetime
 import json
 import logging
 import requests
@@ -9,28 +8,29 @@ import time
 
 from Legobot.Lego import Lego
 from Legobot.Message import Message, Metadata
-from Legobot.Utilities import Utilities
 
 __author__ = "Bren Briggs (fraq)"
 __copyright__ = "Copyright 2017, Legobot"
 
 logger = logging.getLogger(__name__)
 
+
 class Heartbeat(threading.Thread, object):
     def __init__(self, ws, interval):
         self.ws = ws
         self.interval = float(interval) / 1000.0
         self.sequence = 0
-        logger.debug("Heartbeat handler initialized with interval of {}".format(self.interval))
+        logger.debug("Heartbeat handler initialized \
+                     with interval of {}".format(self.interval))
         threading.Thread.__init__(self)
-    
+
     def send(self, ws, seq):
-        payload = {'op':1, 'd':seq}
+        payload = {'op': 1, 'd': seq}
         payload = json.dumps(payload)
         logger.debug("Sending heartbeat with payload {}".format(payload))
         ws.send(payload)
         return
-        
+
     def update_sequence(self, seq):
         self.sequence = seq
         logger.debug("Heartbeat sequence updated to {}".format(self.sequence))
@@ -41,13 +41,14 @@ class Heartbeat(threading.Thread, object):
             self.send(self.ws, self.sequence)
             time.sleep(self.interval)
 
+
 class DiscoBot(threading.Thread, object):
 
     def __init__(self, baseplate, token, actor_urn, *args, **kwargs):
         self.baseplate = baseplate
         self.rest_baseurl = 'https://discordapp.com/api/v6'
         self.token = token
-        self.headers = { "Authorization": "Bot {}".format(token),
+        self.headers = {"Authorization": "Bot {}".format(token),
                         "User-Agent": "Legobot",
                         "Content-Type": "application/json"}
         self.actor_urn = actor_urn
@@ -58,10 +59,11 @@ class DiscoBot(threading.Thread, object):
         return create_connection(self.get_ws_url())
 
     def create_message(self, channel_id, text):
-        baseurl = self.rest_baseurl + '/channels/{}/messages'.format(channel_id)
-        r = requests.post(baseurl,
-                          headers=self.headers,
-                          data=json.dumps({'content': text}))
+        baseurl = self.rest_baseurl + \
+            '/channels/{}/messages'.format(channel_id)
+        requests.post(baseurl,
+                      headers=self.headers,
+                      data=json.dumps({'content': text}))
 
     def identify(self, token):
         payload = {
@@ -78,7 +80,8 @@ class DiscoBot(threading.Thread, object):
             }
         }
         payload['d']['synced_guilds'] = []
-        logger.info("Identifying with the following message: {}".format(payload))
+        logger.info("Identifying with the following message: \
+                    {}".format(payload))
         self.ws.send(json.dumps(payload))
         return
 
@@ -98,11 +101,12 @@ class DiscoBot(threading.Thread, object):
 
     def on_message(self, message):
         if 'content' in message['d']:
-            metadata = self._parse_metadata(message) 
+            metadata = self._parse_metadata(message)
             message = Message(text=message['d']['content'],
                               metadata=metadata).__dict__
             logger.debug(message)
             self.baseplate.tell(message)
+
     def _parse_metadata(self, message):
         metadata = Metadata(source=self.actor_urn).__dict__
         if 'author' in message['d']:
@@ -110,14 +114,15 @@ class DiscoBot(threading.Thread, object):
         else:
             metadata['source_user'] = None
         if 'channel_id' in message['d']:
-            metadata['source_channel'] = message['d']['channel_id']  # this needs to resolve to a human-friendly string
+            metadata['source_channel'] = message['d']['channel_id']
         else:
             metadata['source_channel'] = None
         return metadata
 
     @staticmethod
     def get_ws_url():
-        url = requests.get('https://discordapp.com/api/v6/gateway').json()['url']
+        url = requests.get(
+            'https://discordapp.com/api/v6/gateway').json()['url']
         return url + '?v=6&encoding=json'
 
     def handle(self, message):
@@ -141,6 +146,7 @@ class DiscoBot(threading.Thread, object):
             except json.decoder.JSONDecodeError as e:
                 logger.fatal("No data on socket...")
 
+
 class Discord(Lego):
     '''Lego that builds and connects Legobot.Connectors.Discord.DiscoBot
 
@@ -155,7 +161,7 @@ class Discord(Lego):
     def __init__(self, baseplate, lock, *args, **kwargs):
         super().__init__(baseplate, lock)
         self.botThread = DiscoBot(baseplate, actor_urn=self.actor_urn,
-                                *args, **kwargs)
+                                  *args, **kwargs)
 
     def on_start(self):
         '''Extends pykka's on_start method to launch this as an actor
@@ -174,7 +180,7 @@ class Discord(Lego):
             bool: True if lego is interested in the message.
         '''
 
-        return str(self.botThread) != str(message['metadata']['source'])
+        return str(self.actor_urn) != str(message['metadata']['source'])
 
     def handle(self, message):
         '''Attempts to send a message to the specified destination in Discord.
