e12cc5dc12f2688db1e57c296d747c0de02e6b9e
Bren Briggs <briggs.brenton@gmail.com>
2018-01-02 16:59:42 -0500

    Fix bandit and flake8 errors

--- Test/test_IRC.py
+++ Test/test_IRC.py
@@ -1,22 +1,16 @@
 import unittest
 import threading
-import pykka
-import json
-import os
-import time
-from Legobot.LegoError import LegoError
-from Legobot.Message import Message, Metadata
 from Legobot.Lego import Lego
 from Legobot.Connectors.IRC import IRC
 
-# Mock objects for testing
 
+# Mock objects for testing
 class TestIRC(unittest.TestCase):
     @staticmethod
     def test_initialization():
         baseplate = Lego.start(None, threading.Lock())
         baseplate_proxy = baseplate.proxy()
-        baseplate_proxy.add_child(IRC,
+        baseplate_proxy.add_child(IRC,  # nosec
                                   channels=['#foo'],
                                   nickname='test_nick',
                                   server='foo.testing',
