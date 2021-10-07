101373c6aa9ca34fca18ac880eba0a53084d970c
Ian S Pringle <pard@0x44.pw>
2021-10-07 14:19:10 -0500

    Added click for argv support
    Added a '--config'/'-c' flag for specifiying the location of a config

--- Legobot/__main__.py
+++ Legobot/__main__.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+import click
 from pathlib import Path
 from Legobot.Chatbot import Chatbot
 
-def main():
-    bot = Chatbot(Path(__file__).resolve().parent.joinpath('config.yaml'))
+@click.command()
+@click.option('-c', '--config', default="config.yaml", help="Path to your config, defaults to './config.yaml'")
+def main(config):
+    bot = Chatbot(Path(__file__).resolve().parent.joinpath(config))
     bot.run()
 
 if __name__ == "__main__":
